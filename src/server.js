9793c6d1cabe319344f43654d32f9d2e788e1512
Gabriel Augendre <gabriel@augendre.info>
2016-08-10 16:45:02 +0200

    Add comments

--- src/server.js
+++ src/server.js
@@ -14,16 +14,24 @@ app.use(expressMongoDb(config.db.url));
 app.get('/:number', function (req, res) {
     let uniques = new Set();
     let added  = [];
-    let codes = req.db.collection('codes').find();
 
+    /*
+        If :number parameter contains characters other than digits,
+        number will contain NaN and thus will fail the following test.
+        Therefore, we fall into the else case.
+     */
     let number = Number(req.params.number);
 
     if (number > 0 && number <= 1000) {
+        let codes = req.db.collection('codes').find();
+
         codes.forEach(function (item) {
+            // Fetch data
             uniques.add(item.value);
         }, function () {
             let size = uniques.size;
             while (uniques.size < size + number) {
+                // Generate random numbers and store them
                 let rand = utils.random();
                 if (!uniques.has(rand)) {
                     uniques.add(rand);
@@ -32,7 +40,11 @@ app.get('/:number', function (req, res) {
                     });
                 }
             }
+
+            // Save generated codes
             req.db.collection('codes').insertMany(added);
+
+            // Send them
             res.send(added.map(function (item) {
                 return "c" + item.value;
             }));
